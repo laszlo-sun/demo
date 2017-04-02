0397683d1ee896543f462f4e301d3ee66c5af7ed
laszlo <sunbo@SUNMacBook.local>
2017-04-02 13:35:43 +0900

    test02-04

--- hello.php
+++ hello.php
@@ -1,12 +1,5 @@
 <?
 echo "Hello world!";
 ?>
-aaaaaaaaaaaaa
-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAgit rebase master topicgit rebase master topicgit rebase master topicgit rebase master topicgit rebase master topicgit rebase master topicgit rebase master topicgit rebase master topicgit rebase master topicgit rebase master topicgit rebase master topicgit rebase master topicgit rebase master topicgit rebase master topicgit rebase master topicgit rebase master topicgit rebase master topicgit rebase master topicgit rebase master topicgit rebase master topicgit rebase master topicgit rebase master topicgit rebase master topicgit rebase master topicgit rebase master topicgit rebase master topicgit rebase master topicgit rebase master topicgit rebase master topicgit rebase master topic
-
-ZZZZZZZZZZZZZZZZZZZZZZZZZZZ
-
-HHHHHHHHH
-HHHHHHHH
 
 ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
